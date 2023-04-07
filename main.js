a22e9cb1218f2b5a6f56cb023fbda08771251733
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-04-07 19:13:54 -0300

    Basic Navigation with ReactRouterDOM

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ const isMac = process.platform === "darwin"
 const isDevEnv = process.env.NODE_ENV === "development"
 
 const createMainWindow = () => {
-    const mainWindow = new BrowserWindow({
-        title: "TodosApp",
-        width: isDevEnv ? 1600 : 800,
-        height: 600
-    })
+  const mainWindow = new BrowserWindow({
+    title: "TodosApp",
+    width: isDevEnv ? 1600 : 800,
+    height: 600
+  })
 
-    if (isDevEnv) {
-        mainWindow.webContents.openDevTools()
-    }
+  if (isDevEnv) {
+    mainWindow.webContents.openDevTools()
+  }
 
-    mainWindow.loadURL("http://localhost:5173")
+  mainWindow.loadURL("http://localhost:5173")
 }
 
 app.whenReady().then(() => {
-    createMainWindow()
+  createMainWindow()
 
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createMainWindow()
-        }
-    })
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow()
+    }
+  })
 })
 
 app.on("window-all-close", () => {
-    if (! isMac) {
-        app.quit()
-    }
+  if (!isMac) {
+    app.quit()
+  }
 })
--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -1,10 +1,54 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
+
+// import App from "./App"
+// import "./index.css"
+
+import DashboardPage from "./pages/DashboardPage"
+import SignInPage from "./pages/SignInPage"
+import SignUpPage from "./pages/SignUpPage"
+
+/*
+    The Router Stuff will probably remain in this file.
+    No reason to change it for only three routes as expected
+    */
+import { createBrowserRouter, RouterProvider, Outlet, Link } from "react-router-dom"
+
+const Root = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/dashboard"}>Dashboard</Link>
+          </li>
+          <li>
+            <Link to={"/signup"}>Sign Up</Link>
+          </li>
+          <li>
+            <Link to={"/signin"}>Sign In</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "dashboard", element: <DashboardPage /> },
+      { path: "signin", element: <SignInPage /> },
+      { path: "signup", element: <SignUpPage /> }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
