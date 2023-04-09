2d6c9234ba16e8e584af291280ac6b7401c20c21
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-04-09 10:56:00 -0300

    Spinner

---
+++ renderer/src/components/Spinner.tsx
@@ -0,0 +1,12 @@
+import "./spinner.css"
+
+const Spinner = () => {
+  return (
+    <div class="loading-spinner-container">
+      <div class="loading-spinner"></div>
+      <div class="loading-text">Loading</div>
+    </div>
+  )
+}
+
+export default Spinner
