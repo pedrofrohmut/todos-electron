52f3b9ac7310d6794af9da110048658b71afb2a5
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-04-13 10:14:05 -0300

    Sign Out on Navbar

--- renderer/src/components/Navbar.tsx
+++ renderer/src/components/Navbar.tsx
@@ -1,18 +1,34 @@
 import { Link } from "react-router-dom"
+import { signout } from "../redux/auth/authThunk"
+import { useTypedDispatch } from "../redux/hooks"
 
 import "./navbar.css"
 
 const Navbar = () => {
-  return (
-    <header>
-      <nav>
-        <Link className="nav-link" to={"/"}>Dashboard</Link>
-        <Link className="nav-link">Sign Out</Link>
-        <Link className="nav-link" to={"/signup"}>Sign Up</Link>
-        <Link className="nav-link" to={"/signin"}>Sign In</Link>
-      </nav>
-    </header>
-  )
+    const dispatch = useTypedDispatch()
+
+    const handleSignOut = () => {
+        dispatch(signout())
+    }
+
+    return (
+        <header>
+            <nav>
+                <Link className="nav-link" to={"/"}>
+                    Dashboard
+                </Link>
+                <a className="nav-link" onClick={handleSignOut}>
+                    Sign Out
+                </a>
+                <Link className="nav-link" to={"/signup"}>
+                    Sign Up
+                </Link>
+                <Link className="nav-link" to={"/signin"}>
+                    Sign In
+                </Link>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
